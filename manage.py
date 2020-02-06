57edb376fe6e8d24219672aa64b178fc256e60db
Zainabzav <zainaba2@illinois.edu>
2020-02-06 16:00:40 -0600

    git fix

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azizzaveri_zainab_django_girls_tutorial.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
